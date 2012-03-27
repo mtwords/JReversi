cbc1d5a3bf99507044fe60fd7f9ec41e12f00e2d
mtwords <aescholi@students.zhaw.ch>
2012-03-27 15:53:50 +0200

    initial commit, gui components created

---
+++ JReversi/src/ch/i10a/reversi/gui/Field.java
@@ -0,0 +1,55 @@
+package ch.i10a.reversi.gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+/**
+ * JPanel that represents a Field of the Reversi board.
+ */
+public class Field extends JPanel {
+
+	public static final int WIDTH = 50;
+
+	private int value;
+
+	public Field(int value) {
+		this.value = value;
+		addMouseListener(new MouseListener());
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		setBackground(Color.GREEN);
+		setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+		setSize(new Dimension(WIDTH, WIDTH));
+
+		if (value < 0) {
+			g.setColor(Color.BLACK);
+			g.drawOval(4, 4, 40, 40);
+			g.setColor(Color.WHITE);
+			g.fillOval(4, 4, 40, 40);
+		} else if (value > 0) {
+			g.setColor(Color.BLACK);
+			g.fillOval(4, 4, 40, 40);
+		}
+	}
+
+	// ----------------- inner classes --------------------
+	private class MouseListener extends MouseAdapter {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			super.mouseClicked(e);
+			// TODO value = -1 / 1;
+			System.out.println(value);
+		}
+	}
+}
---
+++ JReversi/src/ch/i10a/reversi/gui/StarterApplet.java
@@ -0,0 +1,96 @@
+package ch.i10a.reversi.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.BoxLayout;
+import javax.swing.JApplet;
+import javax.swing.JPanel;
+
+/**
+ * Starter class of the game. It is implemented as a JApplet,
+ * therefore it can be used in a browser.
+ */
+public class StarterApplet extends JApplet {
+
+	Board board = new Board();
+
+	@Override
+	public void init() {
+		super.init();
+		add(new LetterPane(), BorderLayout.NORTH);
+		add(new CipherPane(), BorderLayout.WEST);
+		add(board, BorderLayout.CENTER);
+	}
+
+	/**
+	 * Overridden to set a fix size. Could be removed if dynamic size
+	 * is supported.
+	 */
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		setSize(new Dimension(25 + (8 * Field.WIDTH), 25 + (8 * Field.WIDTH)));
+	}
+
+	// --------------- inner classes ------------------
+	/**
+	 * A JPanel, which can be used to name the a - h Fields.
+	 * By now it is set fixed as horizontal.
+	 */
+	private class LetterPane extends JPanel {
+
+		public LetterPane() {
+			initComponents();
+		}
+
+		private void initComponents() {
+			setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			setSize(new Dimension(8 * 50, 25));
+			setPreferredSize(getSize());
+			setMaximumSize(getSize());
+			setMinimumSize(getSize());
+
+			add(GuiUtil.getLabel("", 25, 25));
+			add(GuiUtil.getXOrientedFieldLabel("a"));
+			add(GuiUtil.getXOrientedFieldLabel("b"));
+			add(GuiUtil.getXOrientedFieldLabel("c"));
+			add(GuiUtil.getXOrientedFieldLabel("d"));
+			add(GuiUtil.getXOrientedFieldLabel("e"));
+			add(GuiUtil.getXOrientedFieldLabel("f"));
+			add(GuiUtil.getXOrientedFieldLabel("g"));
+			add(GuiUtil.getXOrientedFieldLabel("h"));
+		}
+
+	}
+
+	/**
+	 * A JPanel, which can be used to name the 1 - 8 Fields.
+	 * By now it is set fixed as vertical.
+	 */
+	private class CipherPane extends JPanel {
+
+		public CipherPane() {
+			initComponents();
+		}
+
+		private void initComponents() {
+			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			setSize(new Dimension(25, 8 * 50));
+			setPreferredSize(getSize());
+			setMaximumSize(getSize());
+			setMinimumSize(getSize());
+
+			add(GuiUtil.getYOrientedFieldLabel("1"));
+			add(GuiUtil.getYOrientedFieldLabel("2"));
+			add(GuiUtil.getYOrientedFieldLabel("3"));
+			add(GuiUtil.getYOrientedFieldLabel("4"));
+			add(GuiUtil.getYOrientedFieldLabel("5"));
+			add(GuiUtil.getYOrientedFieldLabel("6"));
+			add(GuiUtil.getYOrientedFieldLabel("7"));
+			add(GuiUtil.getYOrientedFieldLabel("8"));
+		}
+
+	}
+}
