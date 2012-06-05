41d62e73ee23443a24be5e83269ed4bb98df881d
Rene Kamer <renekamer@gmail.com>
2012-06-05 15:00:56 +0200

    Some comments in ComputerPlayer added

--- JReversi/src/ch/i10a/reversi/gameplay/ComputerPlayer.java
+++ JReversi/src/ch/i10a/reversi/gameplay/ComputerPlayer.java
@@ -19,24 +19,41 @@ public class ComputerPlayer extends PlayerAdapter {
 	public ComputerPlayer() {
 		this(Color.BLACK, 1);
 	}
+	
+	/**
+	 * If a computer player has to be set up differently, use this constructor
+	 * 
+	 * @param Color color: the color to give the computer player, value: the field value for the computer player
+	 */
 	public ComputerPlayer(Color color, int value) {
 		this.color = color;
 		this.value = value;
 	}
 
+	/**
+	 * here, the computer player makes his move. The move method to call from MoveHandler
+	 * depends on the difficulty setting (easy, medium, hard).
+	 * 
+	 */
 	@Override
 	public void move() {
 		Field bestMovableField = null;
+		
+		//if the difficulty is set to "easy", the getRandomMove method is chosen
 		if (ReversiProperties.inst().getIntProperty("difficulty") == SettingsConst.PROP_VALUE_EASY){
 			bestMovableField = MoveHandler.getRandomMove();
 		}
+		//if the difficulty is set to "medium", the getBestMove method is chosen with value false (medium calculation switch)
 		if (ReversiProperties.inst().getIntProperty("difficulty") == SettingsConst.PROP_VALUE_MEDIUM){
 			bestMovableField = MoveHandler.getBestMove(false);
 		}
+		//if the difficulty is set to "hard", the getBestMove method is chosen with value true (hard calculation switch)
 		if (ReversiProperties.inst().getIntProperty("difficulty") == SettingsConst.PROP_VALUE_HARD){
 			bestMovableField = MoveHandler.getBestMove(true);
 		}
 
+		//only if the methods return a field (e.g. not null), a move has to be done. if it returns null, the computer
+		//player passes (passing realized elsewhere)
 		if (bestMovableField != null) {
 			getBoard().doMove(bestMovableField);
 			getBoard().getMoves().add(Move.getMove(bestMovableField.getRowNum(), bestMovableField.getColNum()));
