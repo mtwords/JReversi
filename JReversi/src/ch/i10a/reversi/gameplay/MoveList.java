c447cf59ded63dbd3ca9e966611a3f353c435502
Rene Kamer <renekamer@gmail.com>
2012-06-07 00:02:08 +0200

    MoveList.java commented

--- JReversi/src/ch/i10a/reversi/gameplay/MoveList.java
+++ JReversi/src/ch/i10a/reversi/gameplay/MoveList.java
@@ -27,6 +27,13 @@ public class MoveList<T> extends ArrayList<Move> {
 		"8",
 	};
 
+	/**
+	 * equals method for this class. is true when the moves saved in are equal
+	 * 
+	 * @param Object o to equal with
+	 * 
+	 * @return true if object o is equal with this object
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (! (o instanceof MoveList)) {
@@ -62,23 +69,59 @@ public class MoveList<T> extends ArrayList<Move> {
 
 		String move;
 
+		/**
+		 * gives back a move by taking the overgiven row and col.
+		 * 
+		 * @param int row is a numeric representation of the row in the board
+		 * @param int col is a numeric representation of the column in the board
+		 * 
+		 * @return a Move created from row and col
+		 */
 		public static Move getMove(int row, int col) {
 			return new Move(row, col);
 		}
 
+		/**
+		 * creates a move by taking the overgiven row and col.
+		 * 
+		 * @param int row is a numeric representation of the row in the board
+		 * @param int col is a numeric representation of the column in the board
+		 * 
+		 * @return an ArrayList of Fields that are hit by the move done in the field variable
+		 */
 		private Move(int row, int col) {
 			move = resolveMove(row, col);
 		}
 
+		/**
+		 * resolves the numeric representation in a String representation of the place of the field
+		 * 
+		 * @param int row is a numeric representation of the row in the board
+		 * @param int col is a numeric representation of the column in the board
+		 * 
+		 * @return a String containing the letters and numbers of the concerned field
+		 */
 		private String resolveMove(int row, int col) {
 			return new String(letters[col] + numbers[row]);
 		}
 
+		/**
+		 * gives back the string representation of the move
+		 * 
+		 * @return a String representation of move
+		 */
 		@Override
 		public String toString() {
 			return move;
 		}
 
+		/**
+		 * equals method for this class. is true when the move saved in are equal
+		 * 
+		 * @param Object obj to equal with
+		 * 
+		 * @return true if object obj is equal with this object
+		 */
 		@Override
 		public boolean equals(Object obj) {
 			if (! (obj instanceof Move)) {
