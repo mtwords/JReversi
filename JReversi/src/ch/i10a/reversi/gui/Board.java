5f8df08d70e198b545cb56590744b875d7e4bada
Rene Kamer <renekamer@gmail.com>
2012-06-07 00:33:32 +0200

    Board.java commented

--- JReversi/src/ch/i10a/reversi/gui/Board.java
+++ JReversi/src/ch/i10a/reversi/gui/Board.java
@@ -10,19 +10,34 @@ public class Board extends JPanel implements Cloneable {
 	protected Field[][] fields;
 	protected MoveList<Move> moves = new MoveList<Move>();
 
+	/**
+	 * getter for the fields in this board
+	 * 
+	 * @return an array of Field
+	 */
 	public Field[][] getFields() {
 		return fields;
 	}
+	
+	/**
+	 * setter for the fields in this board
+	 * 
+	 * @param an array of Field
+	 */
 	public void setFields(Field[][] fields) {
 		this.fields = fields;
 	}
 
+	/**
+	 * mainly here for inheritance reasons
+	 */
 	public void doMove(Field field) {
 		// do nothing
 	}
 
 	/**
 	 * Returns a List of Moves that were made throughout the game 
+	 * 
 	 * @return MoveList<Move> of Moves made throughout the game
 	 */
 	public MoveList<Move> getMoves(){
@@ -31,12 +46,18 @@ public class Board extends JPanel implements Cloneable {
 	
 	/**
 	 * Returns the last Move made in the game 
+	 * 
 	 * @return the last made Move 
 	 */
 	public Move getLastMove(){
 		return moves.get(moves.size() - 1);
 	}
 
+	/**
+	 * nulls out all the fields on this board (saving memory)
+	 * 
+	 * @param an array of Field
+	 */
 	public void nullFields() {
 		for (int i = 0; i < fields.length; i++) {
 			for (int j = 0; j < fields[i].length; j++) {
@@ -46,6 +67,11 @@ public class Board extends JPanel implements Cloneable {
 		fields = null;
 	}
 
+	/**
+	 * clones this board to have a non cohesive object
+	 * 
+	 * @param clone is a board containing the same values as the original board
+	 */
 	@Override
 	public Board clone() {
 		Board clone = new Board();
